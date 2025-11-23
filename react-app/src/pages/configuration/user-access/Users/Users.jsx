ef09828cfc517fb5d4bc48e53d15982a7fdaed95
Metodi Tosheski <metodi.tosheski@gmail.com>
2025-11-23 13:01:54 +0100

    refactor users configuration screen to use table and offcanvas

--- react-app/src/pages/configuration/user-access/Users/Users.jsx
+++ react-app/src/pages/configuration/user-access/Users/Users.jsx
@@ -103,17 +103,6 @@ const UsersComponent = () => {
         onSave={() => addEditUser.onSaveHandle(handleSave)}
         roles={roles}
       />
-
-      <div className="row">
-        <div className="col-12">
-          <div className="page-title-box d-sm-flex align-items-center justify-content-between">
-            <div className="page-title-right">
-         
-            </div>
-          </div>
-        </div>
-      </div>
-
       <div className="row">
         <div className="col-lg-12">
           <div className="card">
